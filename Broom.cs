1a54b2f95895bcb5b3eec027bb1a6492b2802135
Elvonia <60554110+Elvonia@users.noreply.github.com>
2024-06-13 10:42:57 -0400

    Initial Commit

---
+++ Broom.cs
@@ -0,0 +1,30 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace PeakSweeper
+{
+    public class Broom : MelonMod
+    {
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            if (buildIndex == 0 || buildIndex == 1 || buildIndex == 37)
+            {
+                return;
+            }
+            CleanSweep();
+        }
+
+        private void CleanSweep()
+        {
+            GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+
+            foreach (GameObject obj in allObjects)
+            {
+                if (obj.layer == 22 || obj.tag == "Snow")
+                {
+                    obj.SetActive(false);
+                }
+            }
+        }
+    }
+}
