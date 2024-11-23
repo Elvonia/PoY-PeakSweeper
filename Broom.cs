1edc18f700cbbfaa402b013e7163af1a1b361a80
Kaden5480 <kaden5480@protonmail.com>
2024-11-23 04:50:03 +0000

    Support BepInEx

--- Broom.cs
+++ Broom.cs
@@ -1,17 +1,62 @@
-﻿using MelonLoader;
+﻿#if BEPINEX
+using BepInEx;
+using UnityEngine.SceneManagement;
+
+#elif MELONLOADER
+using MelonLoader;
+
+#endif
+
 using UnityEngine;
 
 namespace PeakSweeper
 {
+#if BEPINEX
+    [BepInPlugin("com.github.Elvonia.PeakSweeper", "Peak Sweeper", PluginInfo.PLUGIN_VERSION)]
+    public class Broom : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        public void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            CommonSceneLoad(scene.buildIndex, scene.name);
+        }
+
+        public void Log(string message)
+        {
+            Logger.LogInfo(message);
+        }
+
+#elif MELONLOADER
     public class Broom : MelonMod
     {
         public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            CommonSceneLoad(buildIndex, sceneName);
+        }
+
+        public void Log(string message)
+        {
+            MelonLogger.Msg(message);
+        }
+
+#endif
+
+        public void CommonSceneLoad(int buildIndex, string sceneName)
         {
             if (buildIndex == 0 || buildIndex == 1 || buildIndex == 37)
             {
                 return;
             }
-            MelonLogger.Msg($"Scene loaded: {sceneName}");
+            Log($"Scene loaded: {sceneName}");
             CleanSweep();
         }
 
@@ -24,7 +69,7 @@ namespace PeakSweeper
                 {
                     if (obj.name.Contains("Snow") || obj.name.Contains("ShrubberyObstacle"))
                     {
-                        MelonLogger.Msg($"Disabled: {obj.name}");
+                        Log($"Disabled: {obj.name}");
                         obj.SetActive(false);
                     }
                 }
